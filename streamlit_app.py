46cbc50a6bc3afa25df4e6e5dc0247004a7e3253
KAVINK-28 <101707786+KAVINK-28@users.noreply.github.com>
2026-03-26 02:52:50 +0530

    Update streamlit_app.py
    removed space on the insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(ingredients , name_on_order )
-                        values ('""" +ingredients_string+   """', ' """+name_on_order+"""')"""
+                        values ('""" +ingredients_string+   """', '"""+name_on_order+"""')"""
     st.write(my_insert_stmt)
     #st.stop()
 
